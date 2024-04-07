eda3a6b96275b0570ffde8071e28bbf9716ea954
yohann bordin <yohann_bordin@hotmail.com>
2024-04-07 17:56:42 +0200

    Co-authored-by: abolabs <abolabs@users.noreply.github.com>

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,18 +3,22 @@
 namespace App\Controller;
 
 use App\Entity\Produit;
+use App\Form\AdminType;
 use App\Controller\Form;
+use App\Form\AdminSupType;
 use App\Entity\Commentaire;
 use Doctrine\ORM\EntityManager;
 use App\Form\CommentaireClientType;
 use App\Repository\ProduitRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Repository\CommentaireRepository;
+use Doctrine\ORM\Mapping\Id;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
 
 
 class HomeController extends AbstractController
@@ -42,6 +46,8 @@ class HomeController extends AbstractController
     public function produit(int $id,CommentaireRepository $commentaireRepository, EntityManagerInterface $entityManager, ProduitRepository $repo, Request $request): Response  // Nous Injectons dans cette fonction l'identifiant de notre produit
     {
             $produit = $repo->find($id);
+        $produits = $repo->findAll();
+        $arrayProduit = array($produits);
             $commentaire = new Commentaire();
             $commentaire->setProduit($produit)->getProduit()->getId();
             $liste = $commentaireRepository->findAll();
@@ -60,6 +66,8 @@ class HomeController extends AbstractController
         return $this->render('home/produit.html.twig', [
             'produit' => $produit ,
             'liste'=>$liste,
+            'produits'=>$produits,
+            'arrayProduit'=>$arrayProduit,
             'commentaire'=>$commentaire,
             'commentaires'=>$commentaires,
 
@@ -68,14 +76,75 @@ class HomeController extends AbstractController
             
         ]);}
         #[IsGranted("ROLE_ADMIN")]
+      /*  @ParamConverter("produit", options={"id" = "id"})*/
     #[Route('/espaceAdmin', name: 'espaceAdmin')]
-    public function espaceAdmin(): Response
+    public function espaceAdminAjout( ProduitRepository $repo, EntityManagerInterface $entityManager, Request $request): Response
     {
-        
+        $produits = $repo->findAll();
+
+        $produit = new Produit();
+        $form = $this->createForm(AdminType::class, $produit);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()){
+            $produit = $form->getData();
+            $entityManager->persist($produit);
+            $entityManager->flush();
+                    }
+
+                    
 
         return $this->render('home/espaceAdmin.html.twig', [
+            'produit' => $produit ,
+            'produits'=>$produits,
+            'form' => $form->createView()
         ]);
     }
+    #[IsGranted("ROLE_ADMIN")]
+
+    #[Route('/espaceAdminSupp/{id}', name: 'espaceAdminSupp')]
+
+    public function espaceAdminASup(Produit $produit,ProduitRepository $repo, EntityManagerInterface $entityManager): Response
+    {
+        
+        $produits = $repo->findAll();
+        $produits = $produit->getId();
+        
+            $entityManager->remove($produit);
+            $entityManager->flush();
+
+     
+      
+
+            $this->addFlash('message', 'Produit supprimé avec succès');
+            return $this->redirectToRoute('espaceAdmin');
+                    ;
+    }
+    #[IsGranted("ROLE_ADMIN")]
+    #[Route('/modifier', name: 'modifier')]
+    public function espaceAdmin2( CommentaireRepository $commentaireRepository, ProduitRepository $repo, EntityManagerInterface $entityManager, Request $request): Response
+    {
+        
+        $commentaire = new Commentaire();
+        $commentaires = $commentaireRepository->findAll();
+
+        $form = $this->createForm(CommentaireClientType::class, $commentaire);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()){
+            $commentaire = $form->getData();
+            $entityManager->persist($commentaire);
+            $entityManager->flush();
+                    }
+
+                    
+
+        return $this->render('home/modifier.html.twig', [
+            'commentaire'=>$commentaire,
+            'commentaires'=>$commentaires,
+
+            'form' => $form->createView()
+        ]);
+    }
+   
 }
 
    /* public function formulaire(EntityManagerInterface $entityManager, Request $request, $form): Response{
